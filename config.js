f4b914cfa15c35d806eaadaa500a634c47d081cf
Francesco Strazzullo <me@francescostrazzullo.info>
2015-06-20 11:36:09 +0200

    fixed react-router version

--- config.js
+++ config.js
@@ -20,6 +20,7 @@ System.config({
     "babel-runtime": "npm:babel-runtime@5.5.8",
     "core-js": "npm:core-js@0.9.18",
     "react": "npm:react@0.13.3",
+    "react-router": "npm:react-router@0.13.3",
     "github:jspm/nodelibs-buffer@0.1.0": {
       "buffer": "npm:buffer@3.2.2"
     },
@@ -82,6 +83,12 @@ System.config({
     },
     "npm:path-browserify@0.0.0": {
       "process": "github:jspm/nodelibs-process@0.1.1"
+    },
+    "npm:react-router@0.13.3": {
+      "object-assign": "npm:object-assign@2.1.1",
+      "process": "github:jspm/nodelibs-process@0.1.1",
+      "qs": "npm:qs@2.4.1",
+      "react": "npm:react@0.13.3"
     },
     "npm:react@0.13.3": {
       "buffer": "github:jspm/nodelibs-buffer@0.1.0",
