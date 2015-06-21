4c391d2c0e289a2da348d00212ca31a0eb1db9d6
Francesco Strazzullo <me@francescostrazzullo.info>
2015-06-21 21:56:49 +0200

    managed build workflow

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,28 @@
 var gulp = require('gulp');
 var connect = require('gulp-connect');
+var rename = require("gulp-rename");
+var del = require('del');
+var shell = require('gulp-shell');
  
 gulp.task('serve', function() {
   connect.server({
     livereload: true,
     port:8000
   });
+});
+
+gulp.task('build',function(){
+	del(['./build/**/*']);
+
+	gulp.src('./index_prod.html')
+	.pipe(rename("index.html"))
+	.pipe(shell(['jspm bundle-sfx src/main build/app.js']))
+	.pipe(gulp.dest('./build'));
+});
+
+gulp.task('serve-build',['build'],function(){
+  	connect.server({
+  		root:'build',
+		port:8001
+  	});
 });
