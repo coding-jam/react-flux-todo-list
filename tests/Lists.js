d7c9d39627345b43aa142ee76add460afa970a30
Francesco Strazzullo <me@francescostrazzullo.info>
2015-06-30 17:56:53 +0200

    cosmetics

--- tests/Lists.js
+++ tests/Lists.js
@@ -8,66 +8,61 @@ var TestUtils = ReactAddons.addons.TestUtils;
 
 describe('List Component', function() {
 
-            var listComponent;
-            var tableElement;
-            var DUMMY_TODOS = ['first Item', 'second Item'];
+    var listComponent;
+    var tableElement;
+    var tbodyElement;
+    var DUMMY_TODOS = ['first Item', 'second Item'];
 
-            beforeEach(function() {
-                    listComponent = TestUtils.renderIntoDocument( < List > < /List>);
-                        tableElement = TestUtils.findRenderedDOMComponentWithTag(listComponent, "table");
-                    });
+    beforeEach(function() {
+        listComponent = TestUtils.renderIntoDocument( < List > < /List>);
+        tableElement = TestUtils.findRenderedDOMComponentWithTag(listComponent, "table");
+        tbodyElement = TestUtils.findRenderedDOMComponentWithTag(tableElement, "tbody");
+    });
 
+    it('should exists', function() {
+        expect(listComponent).toBeDefined();
+    });
 
-                it('should exists', function() {
-                    expect(listComponent).toBeDefined();
-                });
+    it('should contain a "table" element', function() {
+        expect(tableElement).toBeDefined();
+    });
 
-                it('should contain a "table" element', function() {
-                    expect(tableElement).toBeDefined();
-                });
+    it('should have a single placeholder row when no todos is present', function() {
+        expect(_.isArray(tbodyElement.props.children)).toBe(false);
+        expect(tbodyElement.props.children.type).toBe('tr');
+    });
 
-                it('should have a single placeholder row when no todos is present', function() {
-                    var tbodyElement = TestUtils.findRenderedDOMComponentWithTag(tableElement, "tbody");
+    describe('List Component with data', function() {
 
-                    expect(_.isArray(tbodyElement.props.children)).toBe(false);
-                    expect(tbodyElement.props.children.type).toBe('tr');
-                });
-
-                describe('List Component with data', function() {
-
-                    var tbodyElement;
-
-                    beforeEach(function() {
-                        _.each(DUMMY_TODOS, function(todo) {
-                            Actions.add(todo);
-                        });
-
-                        tbodyElement = TestUtils.findRenderedDOMComponentWithTag(tableElement, "tbody");
-                    });
-
-                    it('every todo object should be represented in a row', function() {
-                        expect(tbodyElement.props.children.length).toBe(DUMMY_TODOS.length);
-                    });
-
-                    it('every todo row should have a "delete" button', function() {
-                        _.each(TestUtils.scryRenderedDOMComponentsWithTag(tbodyElement, 'tr'), function(tr) {
-                            expect(TestUtils.findRenderedDOMComponentWithClass(tr, "btn btn-danger")).toBeDefined();
-                        });
-                    });
-
-                    it('Clicking on a delete button should remove a row', function() {
-
-                        var aDeleteButton = TestUtils.scryRenderedDOMComponentsWithClass(listComponent, "btn btn-danger")[0];
-
-                        TestUtils.Simulate.click(React.findDOMNode(aDeleteButton));
-
-                        expect(tbodyElement.props.children.length).toBe(DUMMY_TODOS.length - 1);
-
-                    });
-
-                    afterEach(function() {
-                        Actions.deleteAll();
-                    });
-                });
-
+        beforeEach(function() {
+            _.each(DUMMY_TODOS, function(todo) {
+                Actions.add(todo);
             });
+        });
+
+        it('every todo object should be represented in a row', function() {
+            expect(tbodyElement.props.children.length).toBe(DUMMY_TODOS.length);
+        });
+
+        it('every todo row should have a "delete" button', function() {
+            _.each(TestUtils.scryRenderedDOMComponentsWithTag(tbodyElement, 'tr'), function(tr) {
+                expect(TestUtils.findRenderedDOMComponentWithClass(tr, "btn btn-danger")).toBeDefined();
+            });
+        });
+
+        it('Clicking on a delete button should remove a row', function() {
+
+            var aDeleteButton = TestUtils.scryRenderedDOMComponentsWithClass(listComponent, "btn btn-danger")[0];
+
+            TestUtils.Simulate.click(React.findDOMNode(aDeleteButton));
+
+            expect(tbodyElement.props.children.length).toBe(DUMMY_TODOS.length - 1);
+
+        });
+
+        afterEach(function() {
+            Actions.deleteAll();
+        });
+    });
+
+});
