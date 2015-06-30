96d6743bb6688e6dc835437008269c558b9f4f18
Francesco Strazzullo <me@francescostrazzullo.info>
2015-06-30 12:58:59 +0200

    Menu Tests

--- tests/Menu.js
+++ tests/Menu.js
@@ -5,7 +5,34 @@ import ReactAddons from "react/addons";
 var TestUtils = ReactAddons.addons.TestUtils;
 
 describe('Menu Component', function(){
+
+	var menuComponent;
+	var ulElement;
+
+	beforeEach(function() {
+	    menuComponent = TestUtils.renderIntoDocument(<Menu></Menu>);
+	    ulElement = TestUtils.findRenderedDOMComponentWithTag(menuComponent, "ul");
+	});
+
+	
     it('should exists', function() {
-    	expect(Menu).toBeDefined();
+    	expect(menuComponent).toBeDefined();
+    });
+
+    it('should contain an ul element', function() {
+    	expect(ulElement).toBeDefined();
+    });
+
+    it('should contain exactly two li element', function() {
+    	var children = ulElement.props.children;
+    	var onlyLiElements = true;
+    	for (var i = 0; i < children.length; i++) {
+    		if(children[i].type !== 'li'){
+    			onlyLiElements = false;
+    			break;
+    		}
+    	};
+    	expect(children.length).toBe(2);
+    	expect(onlyLiElements).toBe(true);
     });
 });
